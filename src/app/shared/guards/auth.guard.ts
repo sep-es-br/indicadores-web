2d2c8baede67234da8b317d6e3deb8711806d472
Bruno Matos <brunomatosbarbosa@hotmail.com>
2024-10-17 14:56:11 -0300

    ajustes no login e incluir todos os anos da gestão no farol de indicadores

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,21 +1,68 @@
 import {
   CanActivateChildFn,
-  UrlSegment,
-  UrlSegmentGroup,
-  UrlTree,
+  Router
 } from '@angular/router';
+import { inject } from '@angular/core';
+import { IProfile } from '../interfaces/profile.interface';
+import { catchError, map, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { ProfileService } from '../services/profile/profile.service';
+import { ToastService } from '../services/toast/toast.service';
 
 export const authGuard: CanActivateChildFn = (route, state) => {
-  const storageToken = sessionStorage.getItem('token');
-  if (!!storageToken) {
-    return true;
+
+  const router = inject(Router);
+  const profileService = inject(ProfileService);
+  const toastService = inject(ToastService);
+  const urlToken = route.queryParamMap.get('urlToken'); 
+
+  if (urlToken) {
+    sessionStorage.setItem('token', urlToken); 
+
+    return profileService.getTokenInfo().pipe(
+      tap((response: IProfile) => {
+        
+        const userProfile = {
+          name: response.name,
+          email: response.email,
+          role: response.role,
+        };
+        if (!response.role.includes('INDICADORES_ADMIN')) {
+          toastService.showToast('error','Acesso negado: Você não tem permissão para acessar essa aplicação');
+          sessionStorage.clear(); 
+          router.navigate(['/login']); 
+          return; 
+        }
+
+        sessionStorage.setItem('user-profile', JSON.stringify(userProfile));
+        router.navigate([state.url.split('?')[0]], {
+          queryParams: {}, 
+          replaceUrl: true, 
+        });
+      }),
+      catchError(() => {
+        toastService.showToast(
+          'error',
+          'Erro ao obter informações do usuário'
+        );
+        sessionStorage.clear();
+        router.navigate(['/login']);
+        return of(false); 
+      }),
+      map(() => true) 
+    );
   }
 
-  const loginPageReroute = new UrlTree(
-    new UrlSegmentGroup([], {
-      primary: new UrlSegmentGroup([new UrlSegment('login', {})], {}),
-    })
-  );
+  const storageToken = sessionStorage.getItem('token');
+  
+  if (!storageToken) {
+    router.navigateByUrl('/login', {
+      state: { authError: 'Você precisa estar logado para acessar essa página.' },
+    });
+    return false; 
+  }
 
-  return loginPageReroute;
+
+  return true; 
 };
+
